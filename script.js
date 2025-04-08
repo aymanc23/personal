3c963f89234cb03aca77af4fe44231365b5f9ac4
aymanc23 <aymancharania123@gmail.com>
2025-04-08 00:20:41 -0500

    new resume

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ function closeSidebar() {
 }
 
 function openResume() {
-    window.open('(NEWER) Ayman_Charania_with_internships  copy.pdf', '_blank');
+    window.open('Newest_updated_resume.pdf', '_blank');
 }
 
 
